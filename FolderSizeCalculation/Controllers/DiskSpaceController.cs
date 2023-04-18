5c34e9cce98326ccf23008d6debbf0997a036ab4
ageeva-olga <olga.ageeva.999@mail.ru>
2023-04-18 15:06:20 +0300

    Add controller and swagger.

---
+++ FolderSizeCalculation/Controllers/DiskSpaceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FolderSizeCalculation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiskSpaceController : Controller
+    {
+        public DiskSpaceController()
+        { }
+
+        [HttpGet("{path}")]
+        public IActionResult GetFiles(string path)
+        {
+            return Ok();
+        }
+    }
+}
--- FolderSizeCalculation/Startup.cs
+++ FolderSizeCalculation/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,12 @@ namespace FolderSizeCalculation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FolderSizeCalculation", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +38,8 @@ namespace FolderSizeCalculation
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FolderSizeCalculation v1"));
             }
             else
             {
@@ -49,7 +57,7 @@ namespace FolderSizeCalculation
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
